0e4ed497866f62d43b889fbc4d11d12a312b70e3
Harsha <nagaharshavardhan.nsm@gmail.com>
2019-04-16 22:41:15 -0700

    Added the reset functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 /*
  * Create a list that holds all of your cards
  */
-
+var cards = ["fa-diamond","fa-paper-plane-o","fa-anchor","fa-bolt","fa-cube",
+            "fa-leaf", "fa-bicycle", "fa-bomb",
+            "fa-diamond","fa-paper-plane-o","fa-anchor","fa-bolt","fa-cube",
+            "fa-leaf", "fa-bicycle", "fa-bomb"];
 
 /*
  * Display the cards on the page
@@ -11,6 +14,9 @@
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
+
+shuffleFunction();
+
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -25,6 +31,37 @@ function shuffle(array) {
     return array;
 }
 
+function shuffleFunction(evt){
+
+    var deck = document.querySelector(".deck");
+    var cardList = document.getElementsByClassName("card");
+    for(i=0;i<cards.length;i++){
+        cardList[i].classList.remove('open', 'show', 'match');
+    }
+    cards = shuffle(cards);
+    
+    
+    for(i=0; i<cards.length; i++){
+
+        while(cardList[i].firstChild){
+            cardList[i].firstChild.remove();
+        }
+        
+        var cardNode = document.createElement('i');
+        cardNode.classList.toggle("fa");
+        cardNode.classList.toggle(cards[i]);
+        cardList[i].appendChild(cardNode);
+        
+    }
+    
+}
+
+
+
+
+
+var reset = document.querySelector('.fa-repeat');
+reset.addEventListener('click', shuffleFunction);
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +73,35 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+/*
+var listOfCards = document.getElementsByClassName('card');
+
+var cardNames = []
+
+for (i=0;i<listOfCards.length;i++){
+    cardNames.push(listOfCards[i].firstElementChild.className.split(" ")[1]);
+} 
+console.log(listOfCards);
+console.log(cardNames);
+*/
+var parentCard = document.querySelector('.deck');
+
+
+function cardClicked(evt){
+
+    if(evt.target.class != 'card')
+    {
+        
+
+        if (evt.target.className == 'card'){
+            
+            evt.target.classList.toggle('open');
+            evt.target.classList.toggle('show');
+            evt.target.classList.toggle('match');
+        }
+    }
+}
+
+
+parentCard.addEventListener('click', cardClicked);
+
