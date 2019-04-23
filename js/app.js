115ac71162d93f908d32e7aa0ccdad7f09f85216
Harsha <nagaharshavardhan.nsm@gmail.com>
2019-04-22 18:28:46 -0700

    Readme file added

--- js/app.js
+++ js/app.js
@@ -6,9 +6,7 @@ var cards = ["fa-diamond","fa-paper-plane-o","fa-anchor","fa-bolt","fa-cube",
             "fa-diamond","fa-paper-plane-o","fa-anchor","fa-bolt","fa-cube",
             "fa-leaf", "fa-bicycle", "fa-bomb"];
 var card1, card2, score=0, successMoves=0, unsuccessMoves=0;
-
 var scoreCard = document.querySelector('.moves');
-
 var starList = document.getElementsByClassName("fa-star");
 /*
  * Display the cards on the page
@@ -19,12 +17,18 @@ var starList = document.getElementsByClassName("fa-star");
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 
-
+// The shuffle function is always called when the page reloads
 cards = shuffle(cards);
 scoreCard.innerText = score.toString();
 scoreCard.nextSibling.textContent = " Moves";
 var deck = document.querySelector(".deck");
 var cardList = document.getElementsByClassName("card");
+
+/* 
+1.Looping through each card 
+2.Removing the child of each card 
+3.Creating new child of each card and assigning the class to each card
+*/ 
 for(let i=0;i<cards.length;i++){
     cardList[i].classList.remove('open', 'show', 'match');
 }
@@ -33,14 +37,122 @@ for(let i=0; i<cards.length; i++){
     while(cardList[i].firstChild){
         cardList[i].firstChild.remove();
     }
-    
     var cardNode = document.createElement('i');
     cardNode.classList.add("fa");
     cardNode.classList.add(cards[i]);
-    cardList[i].appendChild(cardNode);
-    
+    cardList[i].appendChild(cardNode);  
 }
 
+var parentCard = document.querySelector('.deck');
+parentCard.addEventListener('click', cardClicked);
+
+/*
+1.The function cardClicked is called when the user clicks the deck
+2.The cards have to open when the user clicks only on the card.
+3.The two variables card1 and card2 are used to store the card information
+4.If the two cards are matched they are kept open else they are closed.
+5.For every card clicked the score counter is incremented.
+6.For every pair of card the succesMoves counter is incremented.
+7.If the two cards are not matched the unsuccessMoves counter is incremented
+8.The funciton displayRating is called for every card clicked to show the star rating.
+*/
+function cardClicked(evt){
+    //Checks whether the user has clicked a card
+    if(evt.target.className == 'card')
+    {
+        score += 1;
+        scoreCard.innerText = score.toString();
+        // Changing from moves to move for the first move
+        if(score == 1){
+        
+            scoreCard.nextSibling.textContent = " Move";
+        }
+        else{
+            scoreCard.nextSibling.textContent = " Moves";
+        }
+        // Setting the card information to the variable card1
+        if (!card1){
+            card1 = evt.target;
+            card1.classList.add('show');
+            card1.classList.add('open');
+        }
+        // Setting the card information to the variable card2 when there is any card opened
+        else{
+            card2 = evt.target;
+            card2.classList.add('show');
+            card2.classList.add('open');
+        }
+        // Checking the logic only when the two cards are opened
+        if(card1&&card2)
+        { 
+            // Checking whether the two cards have the same symbol
+            // Incrementing the successMoves counter
+            if(card1.firstChild.className == card2.firstChild.className){
+                successMoves += 1;
+                card1.classList.remove('open');
+                card1.classList.add('match');
+                card2.classList.remove('open');
+                card2.classList.add('match'); 
+                card1 = null;
+                card2 = null;   
+            }
+            // Incrementing the unsuccessMoves coutnter
+            // Executes when the two cards are not matched
+            else{
+                unsuccessMoves += 1;
+                setTimeout(function(){
+                    card1.classList.remove('open', 'show');
+                    card2.classList.remove('open', 'show');
+                    card1 = null;
+                    card2 = null;
+                }, 200);
+            }
+        }    
+    }
+    displayRating(successMoves, unsuccessMoves);      
+}
+
+var reset = document.querySelector('.fa-repeat');
+reset.addEventListener('click', shuffleFunction);
+
+// Calls the shuffle function and re-initialises the variables
+function shuffleFunction(evt){
+    clearInterval(time);
+    tick = 0;
+    time = setInterval(Timer, 1000);
+    score = 0;
+    successMoves = 0;
+    unsuccessMoves = 0;
+    scoreCard.innerText = score.toString();
+    scoreCard.nextSibling.textContent = " Moves";
+    for(let i=0;i<cards.length;i++){
+        cardList[i].classList.remove('open', 'show', 'match');
+    }
+    cards = shuffle(cards); 
+    for(let i=0; i<cards.length; i++){
+
+        while(cardList[i].firstChild){
+            cardList[i].firstChild.remove();
+        }
+        
+        var cardNode = document.createElement('i');
+        cardNode.classList.add("fa");
+        cardNode.classList.add(cards[i]);
+        cardList[i].appendChild(cardNode);    
+    }
+    for(let i=0; i<starList.length; i++){
+        if(starList[i].classList.contains("fa-star-o")){
+            starList[i].classList.remove("fa-star-o");
+        }
+        if(starList[i].classList.contains('fa-star-half-o')){
+            starList[i].classList.remove('fa-star-half-o');
+        }
+        starList[i].classList.add("fa-star");
+        
+    }   
+}
+
+// Shuffles the cards
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -55,50 +167,6 @@ function shuffle(array) {
     return array;
 }
 
-function shuffleFunction(evt){
-    clearInterval(time);
-    tick = 0;
-    time = setInterval(Timer, 1000);
-    score = 0;
-    successMoves = 0;
-    unsuccessMoves = 0;
-    scoreCard.innerText = score.toString();
-    scoreCard.nextSibling.textContent = " Moves";
-    // var deck = document.querySelector(".deck");
-    // var cardList = document.getElementsByClassName("card");
-    for(let i=0;i<cards.length;i++){
-        cardList[i].classList.remove('open', 'show', 'match');
-    }
-    cards = shuffle(cards);
-
-    
-    
-    for(let i=0; i<cards.length; i++){
-
-        while(cardList[i].firstChild){
-            cardList[i].firstChild.remove();
-        }
-        
-        var cardNode = document.createElement('i');
-        cardNode.classList.add("fa");
-        cardNode.classList.add(cards[i]);
-        cardList[i].appendChild(cardNode);
-        
-    }
-
-    for(let i=0; i<starList.length; i++){
-        if(starList[i].classList.contains("fa-star-o")){
-            starList[i].classList.remove("fa-star-o");
-        }
-        if(starList[i].classList.contains('fa-star-half-o')){
-            starList[i].classList.remove('fa-star-half-o');
-        }
-        starList[i].classList.add("fa-star");
-        
-    }
-    
-}
-
 var tick = 0;
 var mm = 0;
 var ss = 0;
@@ -106,6 +174,7 @@ var timeElement = document.querySelector(".time");
 
 var time = setInterval(Timer, 1000);
 
+// The function is used to display the timer
 function Timer(){
     tick += 1;
 
@@ -116,73 +185,7 @@ function Timer(){
     timeElement.textContent = text;
 }
 
-var reset = document.querySelector('.fa-repeat');
-reset.addEventListener('click', shuffleFunction);
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-var parentCard = document.querySelector('.deck');
-
-
-function cardClicked(evt){
-    if(evt.target.className == 'card')
-    {
-        score += 1;
-        scoreCard.innerText = score.toString();
-        if(score == 1){
-        
-            scoreCard.nextSibling.textContent = " Move";
-        }
-        else{
-            scoreCard.nextSibling.textContent = " Moves";
-        }
-        if (!card1){
-            card1 = evt.target;
-            card1.classList.add('show');
-            card1.classList.add('open');
-        }
-        else{
-            card2 = evt.target;
-            card2.classList.add('show');
-            card2.classList.add('open');
-        }
-        
-        
-        if(card1&&card2)
-        {
-            
-            if(card1.firstChild.className == card2.firstChild.className){
-                successMoves += 1;
-                card1.classList.remove('open');
-                card1.classList.add('match');
-                card2.classList.remove('open');
-                card2.classList.add('match'); 
-                card1 = null;
-                card2 = null;   
-            }
-            else{
-                unsuccessMoves += 1;
-                setTimeout(function(){
-                    card1.classList.remove('open', 'show');
-                    card2.classList.remove('open', 'show');
-                    card1 = null;
-                    card2 = null;
-                }, 200);
-            }
-        }
-        
-    }
-    displayRating(successMoves, unsuccessMoves);      
-}
-
+// Displays the star rating for each move
 function displayRating(a, b){
     let rating;
     if(a==0 && b==0){
@@ -221,11 +224,14 @@ function displayRating(a, b){
             }
         }
     }
+    // Checking the winning condition
     if(a==8){
         clearInterval(time);
         modalFunction();
     }
 }
+
+// Displays a new window after winning the quiz for greetings
 function modalFunction(){
     var modal = document.querySelector(".modal");
     var span = document.getElementsByClassName("close")[0];
@@ -244,6 +250,13 @@ function modalFunction(){
         }
     }
 }
-
-parentCard.addEventListener('click', cardClicked);
-
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
